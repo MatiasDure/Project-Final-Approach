0926cc50c25cb2f4159791cb7f2efd33b354c949
Omair <513589@student.saxion.nl>
2022-04-26 17:01:29 +0200

    small fixes

--- GXPEngine/Blackhole.cs
+++ GXPEngine/Blackhole.cs
@@ -7,30 +7,33 @@ using GXPEngine;
 
 public class Blackhole:Ball
 {
-    Vector2 position;
     public Blackhole(Vector2 pPos) :base("circle.png",1,1, pPos)
     {
-        position = pPos;
+        
     }
 
     void update()
     {
         Level myLevel = ((MyGame)game).currentLevel;
-        foreach (Ball ball in myLevel.balls)
+        foreach (Ball other in myLevel.balls)
         {
-            if(ball != this)
+            if(other != this)
             {
-                Vector2 difference = ball._position - position;    
+                Vector2 difference = other._position - _position;    
                 float distance = difference.Length();
+                int detectionRange = 400;
+                float gravitationalPull = 0;
 
-                int detectionRange = 400; 
-                float gravitationalPull = -Mathf.Pow((0.01f * distance - (detectionRange / 100)), 3.4f) * (Mathf.Pow(0.01f*detectionRange-4,2)*0.35f+0.15f);
+                if (distance > detectionRange)
+                {
+                    gravitationalPull = -Mathf.Pow((0.01f * distance - (detectionRange / 100)), 3.4f) * (Mathf.Pow(0.01f*detectionRange-4,2)*0.35f+0.15f);
+                }
 
                 float G = 6.67f * Mathf.Pow(10, -11);
                 int Mass = 20;
                 float gravitationalPull2 = G * (Mass * Mathf.Pow(10, 13) / Mathf.Pow(distance, 2));
 
-                ball._velocity += difference.Normalized() * gravitationalPull;
+                other._velocity += difference.Normalized() * gravitationalPull;
             }
         }
     }
