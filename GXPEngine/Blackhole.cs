e1aaaf297314f6b48d69d30ef01291706394a7a2
Omair <513589@student.saxion.nl>
2022-04-26 16:47:10 +0200

    Added math for blackhole gravity (there are two version) gravitationalPull: might give us desired effect gravitationalPull2: physically accurate

--- GXPEngine/Blackhole.cs
+++ GXPEngine/Blackhole.cs
@@ -7,8 +7,32 @@ using GXPEngine;
 
 public class Blackhole:Ball
 {
+    Vector2 position;
     public Blackhole(Vector2 pPos) :base("circle.png",1,1, pPos)
     {
+        position = pPos;
+    }
 
+    void update()
+    {
+        Level myLevel = ((MyGame)game).currentLevel;
+        foreach (var item in myLevel.item)
+        {
+            if(item != this)
+            {
+                Vector2 difference = item.position - position;    
+                float distance = difference.Length();
+
+                int detectionRange = 400; 
+                float gravitationalPull = -Mathf.Pow((0.01f * distance - (detectionRange / 100)), 3.4f) * (Mathf.Pow(0.01f*detectionRange-4,2)*0.35f+0.15f);
+
+                float G = 6.67f * Mathf.Pow(10, -11);
+                int Mass = 20;
+                float gravitationalPull2 = G * (Mass * Mathf.Pow(10, 13) / Mathf.Pow(distance, 2));
+
+                item.velocity += difference.Normalized() * gravitationalPull;
+
+            }
+        }
     }
 }
